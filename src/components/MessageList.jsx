e9d786b9855513eccdfe6bb5ba8118c073add707
Merle Jansen <merlevanjansen@gmail.com>
2019-10-31 21:43:43 +0100

    adding autoscroll to messageList

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Message from './Message';
 import '../index.css';
 
 class MessageList extends React.Component {
+  componentWillUpdate () {
+    const node = ReactDOM.findDOMNode (this);
+    this.shouldScrollToBottom =
+      node.scollTop + node.clientHeight + 100 >= node.scrollHeight;
+  }
+
+  componentDidUpdate () {
+    if (this.shouldScrollToBottom) {
+      const node = ReactDOM.findDOMNode (this);
+      node.scollTop = node.scrollHeight;
+    }
+  }
+
   render () {
     return (
       <div className="message-list">
